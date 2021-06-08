993dace46eccbfc64c0ea73c83821bb7859e42cf
deliciousFish <ikl258794613@gmail.com>
2021-06-08 12:59:58 +0800

    simple web

--- basic/web-ser/server.js
+++ basic/web-ser/server.js
@@ -1,20 +1,34 @@
 const http = require("http");
-const server = http.createServer((req, res) => {
+const { URL } = require("url");
+const fs = require("fs/promises");
+const Promise = require("bluebird");
+const readFileblue = Promise.promisify(fs.readFile);
+
+const server = http.createServer(async (req, res) => {
   console.log("已連線");
   console.log(req.url);
+  //將 url 一般化，移除他的query string非必要的斜線，一率小寫
+  const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLocaleLowerCase();
+  console.log(`path:${path}`);
+  // 處理query string
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  console.log(url.searchParams);
 
   res.statusCode = 200; // 2xx, 3xx, 4xx, 5xx
   res.setHeader("Content-Type", "text/plain;charset=UTF-8");
-
+  //router
   switch (req.url) {
     case "/":
       res.end("首頁");
       break;
     case "/test":
-      res.end("測試頁面");
+      res.setHeader("Content-Type", "text/html;charset=UTF-8");
+      let content = await fs.readFile("test.html");
+      res.end(content);
       break;
     case "/about":
-      res.end("關於我們");
+      let name = url.searchParams.get("name") || "網友"; //||是預設值
+      res.end(`Hi, ${name} 這是關於我們`);
       break;
     default:
       res.writeHead(404);
