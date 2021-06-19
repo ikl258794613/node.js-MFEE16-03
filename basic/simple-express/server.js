633cf088a042338d1f7888ebf8dd27157900e0f1
deliciousFish <ikl258794613@gmail.com>
2021-06-19 10:43:00 +0800

    ditels

--- basic/simple-express/server.js
+++ basic/simple-express/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const moment = require("moment");
+const connection = require("./utils/db");
+//同層的用./utils/db
+
 let today = moment().format();
 let app = express();
 
@@ -33,6 +36,31 @@ app.get("/test", function (req, res) {
   res.send("test");
 });
 
-app.listen(3000, () => {
+app.get("/stock", async function (req, res) {
+  let result = await connection.queryAsync("SELECT * FROM stock");
+  //console.log(`目前有${stocks}這些資料`);
+  //[object Object],[object Object],[object Object],[object Object],[object Object]
+  //console.log 一堆[object Object] 但是在網頁上能顯示? 老師上課也能正常顯示
+  //console.log(`目前有${stocks.stock_name}這些資料`);
+  //目前有undefined這些資料
+  // console.log  stocks.stock_name 結果 undefined ??
+  res.render("stock/list", {
+    stocks: result,
+  });
+});
+
+app.get("/stock/:stockCode", async (req, res) => {
+  let result = await connection.queryAsync(
+    "SELECT * FROM stock_price WHERE stock_id = ? ORDER BY date",
+    req.params.stockCode
+  );
+  res.render("stock/detail", {
+    stockPrices: result,
+  });
+});
+
+app.listen(3000, async () => {
+  //在web server開始時連線db，不寫預設也會幫你連但養成習慣寫一下
+  await connection.connectAsync();
   console.log(`連接到port3000`);
 });
